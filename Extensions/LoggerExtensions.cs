d28a2c4da67bc44328ed0d28eabfb8a21ba55fc1
BnnQ <1v25fa94@gmail.com>
2024-03-01 20:49:29 +0200

    Added logger extensions to easily log actions in controllers

---
+++ Extensions/LoggerExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace viki_01.Extensions;
+
+[SuppressMessage("Usage", "CA2254:Шаблон должен быть статическим выражением")] // warning cannot be solved properly for such functionality and should be ignored
+public static class LoggerExtensions
+{
+    public static void LogActionInformation<T>(this ILogger<T> logger,
+        string httpMethod,
+        string actionName,
+        string message,
+        params object[] args)
+    {
+        logger.LogInformation($"[{httpMethod}] {actionName}: {message}", args);
+    }
+
+    public static void LogActionWarning<T>(this ILogger<T> logger,
+        string httpMethod,
+        string actionName,
+        string message,
+        params object[] args)
+    {
+        logger.LogWarning($"[{httpMethod}] {actionName}: {message}", args);
+    }
+
+    public static void LogActionError<T>(this ILogger<T> logger,
+        string httpMethod,
+        string actionName,
+        string message,
+        params object[] args)
+    {
+        logger.LogError($"[{httpMethod}] {actionName}: {message}", args);
+    }
+
+    public static void LogActionDebug<T>(this ILogger<T> logger,
+        string httpMethod,
+        string actionName,
+        string message,
+        params object[] args)
+    {
+        logger.LogDebug($"[{httpMethod}] {actionName}: {message}", args);
+    }
+
+    public static void LogActionCritical<T>(this ILogger<T> logger,
+        string httpMethod,
+        string actionName,
+        string message,
+        params object[] args)
+    {
+        logger.LogCritical($"[{httpMethod}] {actionName}: {message}", args);
+    }
+}
