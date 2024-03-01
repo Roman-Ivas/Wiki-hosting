be5ab82eea96f73f0c442cef359f1ccf83dee93b
BnnQ <1v25fa94@gmail.com>
2024-03-01 20:29:50 +0200

    Added ClaimsPrincipal extension methods to easily retrieve current user ID

---
+++ Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace viki_01.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int GetId(this ClaimsPrincipal user)
+    {
+        var nameIdentifier = user.Claims.FirstOrDefault(claim => claim.Type.Equals("user_id"))?.Value;
+        if (nameIdentifier is null || !int.TryParse(nameIdentifier, out var id))
+            throw new InvalidOperationException("User is not authorized");
+        
+        return id;
+    }
+}
