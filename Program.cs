7dcaedf84f5b51d3ba4bf510223c56a8a0545079
BnnQ <1v25fa94@gmail.com>
2024-03-01 20:55:08 +0200

    Registered services, authorization policies, mappers

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using viki_01.Authorization;
 using viki_01.Contexts;
+using viki_01.Extensions;
 using viki_01.Services;
+using viki_01.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,12 +33,31 @@ builder.Services.AddCors(options =>
         .AllowCredentials());
 });
 
+builder.Services.AddTransient<ITopicRepository, DatabaseTopicRepository>();
+builder.Services.AddTransient<IPageRepository, DatabasePageRepository>();
+builder.Services.AddTransient<IContributorRepository, DatabaseContributorRepository>();
 builder.Services.AddScoped<IAuthService, SqlDbAuthService>();
+builder.Services.AddKeyedSingleton(ServiceKeys.LoggerSerializerOptions,
+    new JsonSerializerOptions
+    {
+        WriteIndented = false,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        ReferenceHandler = ReferenceHandler.IgnoreCycles
+    });
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options => AuthOptions.ConfigureJwtBearer(options));
 
+builder.Services.AddAuthorization(
+    options =>
+    {
+        options.AddPolicy(name: "AdminOnly", policy => policy.RequireRole("Admin"));
+        options.AddPolicy(name: "ModerationOnly", policy => policy.RequireRole("Admin", "Moderator"));
+        options.AddPolicy(name: "PageUpsert", policy => policy.Requirements.Add(new OperationAuthorizationRequirement { Name = "PageUpsert"}));
+    });
+
 builder.Services.AddSingleton<IUserIdProvider, SignalrEmailBasedUserIdProvider>();
+builder.Services.AddScoped<IAuthorizationHandler, PageAuthorizationHandler>();
 
 builder.Services.AddControllers();
 builder.Services.AddHostedService<TokenCleanerService>();
@@ -40,6 +66,13 @@ builder.Services.AddHostedService<TokenCleanerService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMappers(mapperBuilder =>
+{
+    mapperBuilder.AddMapper<TopicToTopicDtoMapper>()
+        .AddMapper<TopicToTopicUpsertDtoMapper>()
+        .AddMapper<PageToPageDtoMapper>()
+        .AddMapper<PageToPageUpsertDtoMapper>();
+});
 
 var app = builder.Build();
 
