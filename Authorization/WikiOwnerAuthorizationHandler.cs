7404f6c5f1830dfd90aa61e19e6237244edf7c36
BnnQ <1v25fa94@gmail.com>
2024-03-04 14:11:32 +0200

    Added WikiOwnerAuthorizationHandler

---
+++ Authorization/WikiOwnerAuthorizationHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using viki_01.Extensions;
+using viki_01.Services;
+
+namespace viki_01.Authorization;
+
+public class WikiOwnerAuthorizationHandler(IContributorRepository contributorRepository, IWikiRepository wikiRepository) : IAuthorizationHandler
+{
+    public async Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        var pendingRequirements = context.PendingRequirements.ToList();
+
+        foreach (var requirement in pendingRequirements)
+        {
+            if (requirement is not OperationAuthorizationRequirement
+                {
+                    Name: "WikiOwner"
+                }) continue;
+            
+            var userId = context.User.GetId();
+            
+            if (context.Resource is not HttpContext httpContext) continue;
+            if (!httpContext.Request.RouteValues.TryGetValue("id", out var wikiIdObject)) continue;
+            var wikiId = Convert.ToInt32(wikiIdObject);
+            if (wikiId == 0) continue;
+            
+            var wiki = await wikiRepository.GetAsync(wikiId);
+            if (wiki is null) continue;
+            
+            var contributor = await contributorRepository.GetContributor(wikiId, userId);
+            if (contributor is null) continue;
+
+            var contributorRole = contributor.ContributorRole.Name.ToUpper();
+            
+            if (contributorRole == "OWNER" || context.User.IsInRole("Moderator") || context.User.IsInRole("Admin"))
+            {
+                context.Succeed(requirement);
+            }
+        }
+    }
+}
