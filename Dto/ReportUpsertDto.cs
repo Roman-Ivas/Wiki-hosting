226ab4b359a082a6964580309cf4a69dce353888
BnnQ <1v25fa94@gmail.com>
2024-03-04 14:11:53 +0200

    Added report DTO and mapper

---
+++ Dto/ReportUpsertDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace viki_01.Dto;
+
+public class ReportUpsertDto
+{
+    public int UserId { get; set; } = default!;
+    
+    [StringLength(256, MinimumLength = 1)]
+    public string ReportedContentUrl { get; set; } = null!;
+    
+    [StringLength(1000, MinimumLength = 1)]
+    public string Text { get; set; } = null!;
+}
---
+++ Services/ReportToReportDtoMapper.cs
@@ -0,0 +1,35 @@
+﻿using viki_01.Dto;
+using viki_01.Entities;
+
+namespace viki_01.Services;
+
+public class ReportToReportDtoMapper : IMapper<Report, ReportUpsertDto>
+{
+    public ReportUpsertDto Map(Report original)
+    {
+        return new ReportUpsertDto
+        {
+            ReportedContentUrl = original.ReportedContentUrl,
+            Text = original.Text,
+            UserId = original.UserId
+        };
+    }
+
+    public Report Map(ReportUpsertDto transformed)
+    {
+        return new Report
+        {
+            ReportedContentUrl = transformed.ReportedContentUrl,
+            Text = transformed.Text,
+            UserId = transformed.UserId
+        };
+    }
+
+    public void Map(Report source, Report destination)
+    {
+        destination.ReportedContentUrl = source.ReportedContentUrl;
+        destination.Text = source.Text;
+        destination.UserId = source.UserId;
+    }
+    
+}
