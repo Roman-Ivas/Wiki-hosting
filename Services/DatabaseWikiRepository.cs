01c73d5c53077066d0bfab82703a94aa21235fc1
BnnQ <1v25fa94@gmail.com>
2024-03-04 14:11:01 +0200

    Added wiki repository

---
+++ Services/DatabaseWikiRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using viki_01.Contexts;
+using viki_01.Dto;
+using viki_01.Entities;
+
+namespace viki_01.Services;
+
+public class DatabaseWikiRepository(WikiHostingSqlServerContext context, IMapper<Wiki, WikiDto> wikiMapper) : IWikiRepository
+{
+    public async Task<ICollection<Wiki>> GetAllAsync(string? search)
+    {
+        IQueryable<Wiki> wikis = context.Wikis;
+        if (!string.IsNullOrWhiteSpace(search))
+        {
+            wikis = wikis.Where(w => w.Name.Contains(search));
+        }
+
+        return await wikis.ToListAsync();
+    }
+
+    public async Task<Wiki?> GetAsync(int wikiId)
+    {
+        var wiki = await context.Wikis.FirstOrDefaultAsync(w => w.Id == wikiId);
+        return wiki;
+    }
+
+    public async Task AddAsync(Wiki wiki)
+    {
+        await context.AddAsync(wiki);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task EditAsync(int wikiId, Wiki editedWiki)
+    {
+        var wiki = await context.Wikis.FirstOrDefaultAsync(w => w.Id == wikiId);
+        if (wiki is null) throw new InvalidOperationException($"Wiki with ID {wikiId} not found");
+
+        wikiMapper.Map(editedWiki, wiki); 
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int wikiId)
+    {
+        var wiki = await context.Wikis.FirstOrDefaultAsync(w => w.Id == wikiId);
+        if (wiki is null) throw new InvalidOperationException($"Wiki with ID {wikiId} not found");
+        
+        context.Wikis.Remove(wiki);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ Services/IWikiRepository.cs
@@ -0,0 +1,12 @@
+﻿using viki_01.Entities;
+
+namespace viki_01.Services;
+
+public interface IWikiRepository
+{
+    public Task<ICollection<Wiki>> GetAllAsync(string? search);
+    public Task<Wiki?> GetAsync(int wikiId);
+    public Task AddAsync(Wiki wiki);
+    public Task EditAsync(int wikiId, Wiki editedWiki);
+    public Task DeleteAsync(int wikiId);
+}
