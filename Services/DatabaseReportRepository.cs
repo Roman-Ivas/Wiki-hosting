403498c150528a00055da6921702d55a21fcbd3a
BnnQ <1v25fa94@gmail.com>
2024-03-04 14:11:08 +0200

    Added report repository

---
+++ Services/DatabaseReportRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using viki_01.Contexts;
+using viki_01.Entities;
+
+namespace viki_01.Services;
+
+public class DatabaseReportRepository(WikiHostingSqlServerContext context) : IReportRepository
+{
+    public async Task<Report?> GetAsync(int id)
+    {
+        return await context.Reports.FirstOrDefaultAsync(x => x.Id == id);
+    }
+    
+    public async Task<ICollection<Report>> GetAllAsync(int? userId = null, string? contentUrl = null)
+    {
+        IQueryable<Report> reports = context.Reports;
+
+        if (userId is not null)
+        {
+            reports = reports.Where(x => x.UserId == userId);
+        }
+        
+        if (contentUrl is not null)
+        {
+            reports = reports.Where(x => x.ReportedContentUrl == contentUrl);
+        }
+
+        return await reports.ToListAsync();
+    }
+
+    public async Task CreateAsync(Report report)
+    {
+        await context.Reports.AddAsync(report);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ Services/IReportRepository.cs
@@ -0,0 +1,10 @@
+﻿using viki_01.Entities;
+
+namespace viki_01.Services;
+
+public interface IReportRepository
+{
+    public Task<Report?> GetAsync(int id);
+    public Task<ICollection<Report>> GetAllAsync(int? userId = null, string? contentUrl = null);
+    public Task CreateAsync(Report report);
+}
