6e5d544e490939df91abf0b515c439fc5c895abc
BnnQ <1v25fa94@gmail.com>
2024-04-02 17:38:54 +0200

    Added DTOs for feed

---
+++ Models/Dto/PageFeedDto.cs
@@ -0,0 +1,19 @@
+﻿namespace viki_01.Models.Dto;
+
+public class PageFeedDto
+{
+    public int Id { get; set; }
+
+    public int WikiId { get; set; }
+
+    public AuthorDto Author { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime EditedAt { get; set; }
+
+    public string Title { get; set; } = null!;
+    public string ImagePath { get; set; } = null!;
+
+    public int NumberOfLikes { get; set; }
+    public int NumberOfComments { get; set; }
+}
