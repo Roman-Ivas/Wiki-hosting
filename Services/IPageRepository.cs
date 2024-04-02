fb88edc879e1662cf9a20a8cdea48e6d65777827
BnnQ <1v25fa94@gmail.com>
2024-04-02 17:47:13 +0200

    Repositories improvements

--- Services/IPageRepository.cs
+++ Services/IPageRepository.cs
@@ -1,10 +1,12 @@
 ﻿using viki_01.Entities;
+using viki_01.Models.Dto;
 
 namespace viki_01.Services;
 
 public interface IPageRepository
 {
     public Task<ICollection<Page>> GetAllAsync(int wikiId);
+    public Task<ICollection<PageFeedDto>> GetRelevantPagesAsync(int limit = 20);
     public Task<Page?> GetAsync(int id);
     public Task AddAsync(Page page);
     public Task EditAsync(int id, Page editedPage);
