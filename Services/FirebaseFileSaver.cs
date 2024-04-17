b9a99ab5252d04b11b1ee9dd0b1c81d9c20001d4
BnnQ <1v25fa94@gmail.com>
2024-04-17 17:43:16 +0200

    Added IFileSaver with implementation via Firebase blob storage

---
+++ Services/FirebaseFileSaver.cs
@@ -0,0 +1,19 @@
+﻿using Firebase.Storage;
+
+namespace viki_01.Services;
+
+public class FirebaseFileSaver(string bucket) : IFileSaver
+{
+    private readonly FirebaseStorage storage = new(bucket);
+
+    public async Task<string> SaveFileAsync(IFormFile file, string? bucketName = null)
+    {
+        await using var stream = file.OpenReadStream();
+        var fileExtension = file.FileName.Split('.').Last();
+        var fileName = Guid.NewGuid().ToString("N") + '.' + fileExtension;
+        var filePath = !string.IsNullOrWhiteSpace(bucketName) ? bucketName + '/' + fileName : fileName;
+        await storage.Child(filePath).PutAsync(stream);
+
+        return await storage.Child(filePath).GetDownloadUrlAsync();
+    }
+}
---
+++ Services/IFileSaver.cs
@@ -0,0 +1,6 @@
+﻿namespace viki_01.Services;
+
+public interface IFileSaver
+{
+    Task<string> SaveFileAsync(IFormFile file, string? bucketName = null);
+}
