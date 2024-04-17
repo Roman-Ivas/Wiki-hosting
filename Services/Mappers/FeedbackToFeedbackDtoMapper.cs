ae40c42772f89a0d2be42acd78a0a5d37f243859
BnnQ <1v25fa94@gmail.com>
2024-04-17 17:38:44 +0200

    Added Feedback entity with FeedbackDto and mapper

---
+++ Services/Mappers/FeedbackToFeedbackDtoMapper.cs
@@ -0,0 +1,31 @@
+﻿using viki_01.Entities;
+using viki_01.Models.Dto;
+
+namespace viki_01.Services.Mappers;
+
+public class FeedbackToFeedbackDtoMapper : IMapper<Feedback, FeedbackDto>
+{
+    public FeedbackDto Map(Feedback original)
+    {
+        return new FeedbackDto
+        {
+            Text = original.Text,
+            UserId = original.UserId
+        };
+    }
+
+    public Feedback Map(FeedbackDto transformed)
+    {
+        return new Feedback
+        {
+            Text = transformed.Text,
+            UserId = transformed.UserId
+        };
+    }
+
+    public void Map(Feedback source, Feedback destination)
+    {
+        destination.Text = source.Text;
+        destination.UserId = source.UserId;
+    }
+}
