4095e022fdcf4e5f1aed2cabb0f050ae087b147c
r4yz7 <mmischenko850@gmail.com>
2024-04-18 12:31:18 +0300

    get page by wiki id

--- Controllers/WikiController.cs
+++ Controllers/WikiController.cs
@@ -12,7 +12,7 @@ namespace viki_01.Controllers;
 public class WikiController(IWikiRepository wikiRepository, ILoggerFactory loggerFactory) : ControllerBase
 {
     private readonly ILogger<WikiController> logger = loggerFactory.CreateLogger<WikiController>();
-    
+   
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> GetWikis([FromServices] IMapper<Wiki, WikiDto> mapper, [FromQuery] string? search = null)
@@ -31,7 +31,7 @@ public class WikiController(IWikiRepository wikiRepository, ILoggerFactory logge
         logger.LogActionInformation(HttpMethods.Get, nameof(GetWikis), "Wikis found and succesfully returned");
         return Ok(mapper.Map(wikis));
     }
-    
+
     [HttpGet("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -221,5 +221,5 @@ public class WikiController(IWikiRepository wikiRepository, ILoggerFactory logge
         logger.LogActionInformation(HttpMethods.Delete, nameof(RemoveWikiContributor), "Succesfully removed contributor with ID: {userId} from wiki with ID: {id}", userId, id);
         return NoContent();
     }
-    
+
 }
