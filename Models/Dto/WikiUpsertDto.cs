bd6d8a80396ef6f44d0f2ac1345b6ba99e95ac01
BnnQ <1v25fa94@gmail.com>
2024-04-18 10:17:58 +0200

    Added MainLinks and BackgroundImagePath to Wiki entity and DTO

--- Models/Dto/WikiUpsertDto.cs
+++ Models/Dto/WikiUpsertDto.cs
@@ -6,4 +6,9 @@ public class WikiUpsertDto
 {
     [StringLength(100, MinimumLength = 1)]
     public string Name { get; set; } = null!;
+    
+    [StringLength(512, MinimumLength = 1)]
+    public string BackgroundImagePath { get; set; } = null!;
+    
+    public IEnumerable<LinkDto> MainLinks { get; set; } = new List<LinkDto>();
 }
